c703d0a782a6531c25ec2ea2a8a67880df8dc212
PrinceNickBALLO <prince@zmooz.com>
2023-02-10 10:56:33 +0100

    fix Call to a member function removeAttribute() on bool

--- src/Optimizer/Transformer/ServerSideRendering.php
+++ src/Optimizer/Transformer/ServerSideRendering.php
@@ -165,7 +165,9 @@ final class ServerSideRendering implements Transformer
             // Removal of attributes is deferred as layout application needs them.
             if (is_array($attributesToRemove)) {
                 foreach ($attributesToRemove as $attributeToRemove) {
-                    $adaptedElement->removeAttribute($attributeToRemove);
+                    if(gettype($attributeToRemove) != "boolean"){
+                        $adaptedElement->removeAttribute($attributeToRemove);
+                    }
                 }
             }
         }
